6c5317bba830ce43e24f0e69b4d0e6bf3df4d51e
nightblade9 <nightbladecodes@gmail.com>
2022-12-16 12:55:17 -0500

    WORKS with the DLLs here and NVDA speech activated

--- scenes/Main.gd
+++ scenes/Main.gd
@@ -15,3 +15,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_Button_pressed():
+	TTS.speak("Clicked the button")
