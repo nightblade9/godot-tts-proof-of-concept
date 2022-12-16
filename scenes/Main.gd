071b9274d09610e45a9f05a294ea2e80562097c4
Ashiq Alibhai <ashiq.alibhai@phreesia.com>
2022-12-16 12:46:11 -0500

    Initial TTS experiment

---
+++ scenes/Main.gd
@@ -0,0 +1,17 @@
+extends Node
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	TTS.speak("Hello, world.")
+	
+	$VBoxContainer/Label.text = "Can detect reader: %s\nHas reader: %s" % [
+		TTS.can_detect_screen_reader, TTS.has_screen_reader
+	]
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
